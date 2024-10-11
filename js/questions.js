9eefabe32dba4cf250b97b0b4de553a18e66e450
BILLIONAIRE-SAGE <apeabahbeatrice@gmail.com>
2024-10-11 14:32:53 +0000

    hh

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the basic unit of life?",
+    answer: "Cell",
+    options: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which organ is primarily responsible for detoxifying chemicals and metabolizing drugs?",
+    answer: "Liver",
+    options: [
+      "Heart",
+      "Liver",
+      "Kidney",
+      "Stomach"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the process by which plants convert sunlight into chemical energy?",
+    answer: "Photosynthesis",
+    options: [
+      "Photosynthesis",
+      "Respiration",
+      "Fermentation",
+      "Transpiration"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the unit of force in the International Systems of Units(SI)?",
+    answer: "Newton",
+    options: [
+      "Joule",
+      "Pascal",
+      "Newton",
+      "Watt"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following best describes Newton's First Law of Motion?",
+    answer: "An object at rest stays at rest, and an object in motion stays in motion unless acted upon by a force.",
+    options: [
+      "Energy cannot be created or destroyed.",
+      "For every action, there is an equal and opposite reaction",
+      "Force equals mass times acceleration",
+      "An object at rest stays at rest, and an object in motion stays in motion unless acted upon by a force."
+    ]
+  },
+];
